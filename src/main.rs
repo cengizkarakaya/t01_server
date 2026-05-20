4a8ea26ac6cb1e005d60c9eb26a7ef9715757823
cengizkarakaya <cengizkarakaya@hotmail.com>
2026-05-20 11:50:52 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod heartbeat;
 
-fn main() -> std::io::Result<()> {
+fn main() {
+
     println!("{}", "\n\x1b[38;2;0;135;255mt01 den merhaba!!!");
-    heartbeat::heartbeat()?;
-    Ok(())
+
+    if let Err(error) = heartbeat::heartbeat() {
+        eprintln!("Heartbeat hatası: {error}");
+    }
+
 }
